5d712132097e93f8bca0460007905c78a592e631
areekaras <shibili.areekara@gmail.com>
2025-05-18 12:20:34 +0800

    Create myPMSApp in xcode with multiplatform app

---
+++ myPMSApp/myPMSApp/myPMSAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myPMSAppApp.swift
+//  myPMSApp
+//
+//  Created by Shibili Areekara on 18/05/25.
+//
+
+import SwiftUI
+
+@main
+struct myPMSAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myPMSApp/myPMSAppTests/myPMSAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  myPMSAppTests.swift
+//  myPMSAppTests
+//
+//  Created by Shibili Areekara on 18/05/25.
+//
+
+import Testing
+
+struct myPMSAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
