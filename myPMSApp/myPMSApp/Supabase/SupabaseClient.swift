1d49183a1c9ae2495272447927e86d81c4ff5c91
areekaras <shibili.areekara@gmail.com>
2025-05-20 07:01:13 +0800

    Add supabase package

---
+++ myPMSApp/myPMSApp/Supabase/SupabaseClient.swift
@@ -0,0 +1,8 @@
+import Foundation
+import Supabase
+
+let supabase = SupabaseClient(
+    supabaseURL: URL(string: "https://dmmpqfixtgdanpgvofzr.supabase.co")!,
+    supabaseKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRtbXBxZml4dGdkYW5wZ3ZvZnpyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDc2MDg4NzYsImV4cCI6MjA2MzE4NDg3Nn0.B3K6kBnNW3DIqwiRCxLP7qcSlUyLq-BK1Vsc9C7sXoA"
+)
+
