a2d4151cbfce2b986db23b4c758c5e739f366242
areekaras <shibili.areekara@gmail.com>
2025-05-23 17:15:47 +0800

    Load supabase API config data from configuration file

---
+++ myPMSApp/myPMSApp/Environment.swift
@@ -0,0 +1,19 @@
+import Foundation
+
+enum AppEnvironment {
+    
+    static let supabaseURL: URL = {
+        guard let urlString = Bundle.main.object(forInfoDictionaryKey: "SUPABASE_URL") as? String,
+              let url = URL(string: urlString) else {
+            fatalError("SUPABASE_URL not set in config")
+        }
+        return url
+    }()
+    
+    static let supabaseKey: String = {
+        guard let key = Bundle.main.object(forInfoDictionaryKey: "SUPABASE_KEY") as? String else {
+            fatalError("SUPABASE_KEY not set in config")
+        }
+        return key
+    }()
+}
